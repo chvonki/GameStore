d171235afbcd292e540db06c3fed5ea61de323d7
Екатерина Богаченко <katyabo@list.ru>
2024-10-03 21:04:07 +0300

    Добавлена функция удаления Игры, а так же модальное окно для подтверждения удаления

--- GameStore.Client/Models/GameClient.cs
+++ GameStore.Client/Models/GameClient.cs
@@ -30,14 +30,35 @@ public static class GameClient
         }
     };
 
-    public static List<Game> GetGames()
+    public static Game[] GetGames()
     {
-        return games;
+        return games.ToArray();
     }
 
     public static void AddGame(Game game)
     {
         game.Id = games.Max(game => game.Id) + 1;
         games.Add(game);
+    }
+
+    public static Game GetGame(int id)
+    {
+        return games.Find(game => game.Id == id) ?? throw new Exception("Игра не найдена!");
+    }
+
+    public static void UpdateGame(Game updatedGame)
+    {
+        Game existingGame = GetGame(updatedGame.Id);
+        existingGame.Name = updatedGame.Name;
+        existingGame.Genre = updatedGame.Genre;
+        existingGame.Price = updatedGame.Price;
+        existingGame.ReleaseDate = updatedGame.ReleaseDate;
+
+    }
+
+    public static void DeleteGame(int id)
+    {
+        Game game = GetGame(id);
+        games.Remove(game);
     }
 }
